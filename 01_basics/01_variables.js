a181cfc999411e02c778b4724c7b1df097878751
Rohanaga <115742983+Rohanaga@users.noreply.github.com>
2024-06-03 19:05:42 +0000

    all changes commited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accID = 123556
+var accPassword = "123450"
+let accmailID = "agarwalrohan447@gmail.com"
+acccity = "agra"
+let accState;
+
+//accID = 13// not allowed
+accmailID = "agarwalrohan492@gmail.com"
+accPassword = "852963"
+acccity = "modinagar"
+
+
+console.log(accID);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accID, accPassword, accmailID, acccity, accState])
