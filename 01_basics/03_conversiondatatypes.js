b1f5d8975d739b062600728e0cec1360788674bc
Rohanaga <115742983+Rohanaga@users.noreply.github.com>
2024-06-03 20:13:19 +0000

    conversion operation

--- 01_basics/03_conversiondatatypes.js
+++ 01_basics/03_conversiondatatypes.js
@@ -9,7 +9,7 @@ let valueInNumber = Number(score)
 // true => 1; false => 0
 let isLoggedIn="hitesh"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -18,5 +18,45 @@ console.log(booleanIsLoggedIn);
 let somenumber = 33
 
 let Stringnumber = String(somenumber)
-console.log(Stringnumber);
-console.log(typeof Stringnumber);
+//console.log(Stringnumber);
+//console.log(typeof Stringnumber);
+
+// *************************Operations*****************************
+
+let val = 3
+let negvalue = -val
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " rohan"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log((3 + 4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2 , num3
+ num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 101
+counter =  gamecounter++;
+console.log(gamecounter);// this will increase
+console.log(counter); // it will remain same as it is postfix
+
+// links to study
+// https://tc39.es/ecma262/multipage/abstract-operations
+ //html#sec-type-conversion
