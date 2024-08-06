f80e14eb0589b47df2dca323b0b242566f1fd298
Rohanaga <115742983+Rohanaga@users.noreply.github.com>
2024-08-06 18:54:00 +0000

    promises

---
+++ 10_advanced_one/promises.js
@@ -0,0 +1,28 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // BD calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve() 
+    }, 1000 )
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve,  reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    })
+})
